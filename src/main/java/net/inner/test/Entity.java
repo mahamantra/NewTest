8a32791bf2c3861a7c838c26b57d3e20c5c05488
Igor Kim <kim-igor1@yandex.ru>
2019-12-25 20:02:39 +0400

    adding new class Entity

---
+++ src/main/java/net/inner/test/Entity.java
@@ -0,0 +1,22 @@
+package net.inner.test;
+
+public class Entity {
+    private int i;
+
+    public int getI() {
+        return i;
+    }
+
+    public void setI(int i) {
+        this.i = i;
+    }
+
+    @Override
+    public String toString() {
+        return "Entity{" +
+                "i=" + i +
+                '}';
+    }
+
+
+}
